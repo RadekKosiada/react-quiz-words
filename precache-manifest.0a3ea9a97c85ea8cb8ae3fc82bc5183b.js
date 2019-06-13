ae32710a87b35e5590ea6f5055356b5791436933
RadekKosiada <kosiadar@gmail.com>
2019-06-13 18:56:08 +0200

    Updates

---
+++ precache-manifest.0a3ea9a97c85ea8cb8ae3fc82bc5183b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758223d6ccd09d4bdcfb0c25b5c0ac25",
+    "url": "/react-quiz-words/index.html"
+  },
+  {
+    "revision": "9ad611621e4630b7d01a",
+    "url": "/react-quiz-words/static/css/main.837f8d10.chunk.css"
+  },
+  {
+    "revision": "3a2a332218746b4bda03",
+    "url": "/react-quiz-words/static/js/2.a10f2e64.chunk.js"
+  },
+  {
+    "revision": "9ad611621e4630b7d01a",
+    "url": "/react-quiz-words/static/js/main.8a0ac36a.chunk.js"
+  },
+  {
+    "revision": "2d762acf9aca03b7d583",
+    "url": "/react-quiz-words/static/js/runtime~main.8460acf8.js"
+  }
+]);
