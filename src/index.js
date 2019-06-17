f4fc31f36e81d7e6a58c4d978949ae631739b1f9
RadekKosiada <kosiadar@gmail.com>
2019-06-17 20:27:16 +0200

    Connect the reduc tool & fix the reducer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { createStore } from 'redux';
 import quizApp from './reducer';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(quizApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,14 @@
 const initialState = {
   score: 0
-}
+};
 
-function quizApp(state= initialState, action) {
-  switch(action.type) {
-    case INCREMENT_SCORE:
+export default function quizApp(state = initialState, action) {
+  switch (action.type) {
+    case 'INCREMENT_SCORE':
       return Object.assign({}, state, {
         score: state.score++
-      })
-      default:
-        return state
+      });
+    default:
+      return state;
   }
 }
