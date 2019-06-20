8583e3cb269841881950efd080355a3b5eb6bd7a
RadekKosiada <kosiadar@gmail.com>
2019-06-20 16:57:09 +0200

    Style radio buttons & move Sass variables into their file

--- src/components/inputForm.js
+++ src/components/inputForm.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "../App.scss";
+import "../radioButton.scss";
 import axios from "axios";
 
 export default class InputForm extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    
   }
   componentDidMount() {
     console.log(this.props.round, this.props.allTasks);
@@ -17,48 +17,48 @@ export default class InputForm extends Component {
       <div>
         <div>
           <p className="title">Words:</p>
-          {!currentSet && (<p>Loading....</p>)}
-          {currentSet && (<p>{currentSet.quiz.map((elem, index) => (
-          <span key={index}>{elem} </span>
-          ))}</p>)}
+          {!currentSet && <p>Loading....</p>}
+          {currentSet && (
+            <p>
+              {currentSet.quiz.map((elem, index) => (
+                <span key={index}>{elem} </span>
+              ))}
+            </p>
+          )}
 
           <p className="title">Your answer:</p>
-          {!currentSet && (<p>Loading....</p>)}
+          {!currentSet && <p>Loading....</p>}
           {currentSet && (
             <form className="" onSubmit={this.props.handleSubmit}>
               {currentSet.option.map((elem, index) => (
                 <div className="radio" key={index}>
-                  {/* <label>
-                    <input 
-                      type="radio" 
+                  <label>
+                    <input
+                      type="radio"
                       name="quiz"
-                      value={index} 
-                      checked={this.state.selected} 
-                      onChange={this.props.handleChange} />
+                      value={index}
+                      checked={this.state.selected}
+                      onChange={this.props.handleChange}
+                    />
                     {elem}
-                  </label>  */}
+                  </label>
                   {/* TESTING */}
                 </div>
               ))}
               <input className="button" type="submit" value="Submit" />
             </form>
           )}
-           <label>
-                  <input 
-                        type="radio"
-                        name="quiz"
-                        value="Pizza" />
-                        Pizza
-                        </label>
-                        <label>
-                  <input 
-                        type="radio"
-                        name="quiz"
-                        value="Pasta" />
-                        Pasta
-                        </label>
+          <label>
+            <input type="radio" name="quiz" value="Pizza" />
+            <span className="checkmark" />
+            Pizza
+          </label>
+          <label>
+            <input type="radio" name="quiz" value="Pasta" />
+            <span className="checkmark" />
+            Pasta
+          </label>
         </div>
-
       </div>
     );
   }
