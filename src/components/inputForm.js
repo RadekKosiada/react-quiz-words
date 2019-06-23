cfefe02c3c1abd55d86bd555b1d7b08e3d7b8469
RadekKosiada <kosiadar@gmail.com>
2019-06-23 23:45:42 +0200

    Adjust grid in inputForm

--- src/components/inputForm.js
+++ src/components/inputForm.js
@@ -12,61 +12,42 @@ export default class InputForm extends Component {
   }
   render() {
     const currentSet = this.props.allTasks[this.props.round - 1];
-    
-    if(currentSet) {
+
+    if (currentSet) {
       return (
-    //   <div className="words">
-    //   {currentSet.quiz.map((elem, index) => (
-    //     <p key={index}>
-    //       {elem}
-    //     </p>
-    //   ))}
-    // </div>
-    // <div className="answer">
-        
-      
-          <form onSubmit={this.props.handleSubmit}>
-            {currentSet.option.map((elem, index) => (
-              <div className="radio" key={index}>
-                <label>
-                  <input
-                    type="radio"
-                    name="quiz"
-                    value={index}
-                    checked={this.state.selected}
-                    onChange={this.props.handleChange}
-                  />
-                  <span className="checkmark" />
-                  {elem}
-                </label>
-              </div>
-            ))}
-            <input className="button" type="submit" value="Submit" />
-          </form>
-        )
-      
-      
-      
+        <form className="answer" onSubmit={this.props.handleSubmit}>
+          {currentSet.option.map((elem, index) => (
+            <label className="radio" key={index}>
+              <input
+                type="radio"
+                name="quiz"
+                value={index}
+                checked={this.state.selected}
+                onChange={this.props.handleChange}
+              />
+              <span className="checkmark" />
+              {elem}
+            </label>
+          ))}
+          <input className="button" type="submit" value="Submit" />
+        </form>
+      );
     } else {
-
-      return (
-      <p className="loading">Loading....</p>
-      )
+      return <p className="loading">Loading....</p>;
     }
-    
-      // <div className="words-container">
-      //   {/* fourth row */}
-      //     {!currentSet && (
-      //       <p className="loading">Loading....</p>)
-      //    }
 
-      //     {currentSet && (
-            
-      //     )}
-       
-      //   {/* fifth row */}
-      
-      // </div>
-    
+    // <div className="words-container">
+    //   {/* fourth row */}
+    //     {!currentSet && (
+    //       <p className="loading">Loading....</p>)
+    //    }
+
+    //     {currentSet && (
+
+    //     )}
+
+    //   {/* fifth row */}
+
+    // </div>
   }
 }
