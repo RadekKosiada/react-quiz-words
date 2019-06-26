fa6955136a8ab50abab4537294adddd68ba37dc4
RadekKosiada <kosiadar@gmail.com>
2019-06-26 15:11:33 +0200

    Change the grid-gap for 450px

--- src/components/gameOverPopup.js
+++ src/components/gameOverPopup.js
@@ -5,30 +5,31 @@ import "../scss-files/App.scss";
 export default function GameOverPopup(props) {
   return (
     <div>
-      <div className="overlay" />
-      <div className="game-over">
-        {props.timeApp === 0 && (
-          <div>
-            <h4>Too late!</h4>
-            <p>
-              The correct answer is:
-              <br />
-              <span className="title">{props.correctAnswer}</span>
-            </p>
-          </div>
-        )}
+      <div className="overlay">
+        <div className="game-over">
+          {props.timeApp === 0 && (
+            <div>
+              <h4>Too late!</h4>
+              <p>
+                The correct answer is:
+                <br />
+                <span className="title">{props.correctAnswer}</span>
+              </p>
+            </div>
+          )}
 
-        {props.timeApp > 0 && (
-          <div>
-            <h4>Wrong</h4>
-            <p>
-              The correct answer is:
-              <br />
-              <span className="title">{props.correctAnswer}</span>
-            </p>
-          </div>
-        )}
-        <RestartButton restartGamePopup={props.restartGameApp} />
+          {props.timeApp > 0 && (
+            <div>
+              <h4>Wrong</h4>
+              <p>
+                The correct answer is:
+                <br />
+                <span className="title">{props.correctAnswer}</span>
+              </p>
+            </div>
+          )}
+          <RestartButton restartGamePopup={props.restartGameApp} />
+        </div>
       </div>
     </div>
   );
